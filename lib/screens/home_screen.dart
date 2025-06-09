91955e20b3fb9ba32755dadc7fe2d95bd6cff43d
matijepekovic <122064226+matijepekovic@users.noreply.github.com>
2025-06-09 16:49:21 -0700

    Revert "Improve landscape StatCard scaling"

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -422,13 +422,12 @@ class _HomeScreenState extends State<HomeScreen>
       VoidCallback? onTap,
       double width,
       ) {
-    final orientationScale = isLandscape(context) ? 1.2 : 1.0;
     final iconSize = responsiveValue(
       context,
       mobile: 24.0,
       tablet: 28.0,
       desktop: 32.0,
-    ) * orientationScale;
+    );
 
     Widget cardContent = Card(
       elevation: 2,
@@ -482,7 +481,6 @@ class _HomeScreenState extends State<HomeScreen>
                         child: Text(
                           value,
                           style: headlineSmall(context).copyWith(
-                            fontSize: headlineSmall(context).fontSize! * orientationScale,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -496,7 +494,6 @@ class _HomeScreenState extends State<HomeScreen>
                         child: Text(
                           title,
                           style: labelMedium(context).copyWith(
-                            fontSize: labelMedium(context).fontSize! * orientationScale,
                             color: Colors.grey[600],
                             fontWeight: FontWeight.w500,
                           ),
--- test/responsive_mixins_test.dart
+++ test/responsive_mixins_test.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rufko/mixins/responsive_breakpoints_mixin.dart';
-import 'package:rufko/mixins/responsive_dimensions_mixin.dart';
 import 'package:rufko/mixins/responsive_text_mixin.dart';
 
-class _MixinTester
-    with ResponsiveBreakpointsMixin, ResponsiveDimensionsMixin, ResponsiveTextMixin {}
+class _MixinTester with ResponsiveBreakpointsMixin, ResponsiveTextMixin {}
 
 void main() {
   final testerMixin = _MixinTester();
@@ -22,22 +20,5 @@ void main() {
     final context = tester.element(find.byType(SizedBox));
     final size = testerMixin.responsiveFontSize(context, 20, max: 18);
     expect(size, 18);
-  });
-
-  testWidgets('windowClassValue returns expanded', (tester) async {
-    await tester.pumpWidget(
-      const MediaQuery(
-        data: MediaQueryData(size: Size(1300, 800)),
-        child: MaterialApp(home: SizedBox()),
-      ),
-    );
-    final context = tester.element(find.byType(SizedBox));
-    final value = testerMixin.windowClassValue(
-      context,
-      compact: 'c',
-      medium: 'm',
-      expanded: 'e',
-    );
-    expect(value, 'e');
   });
 }
