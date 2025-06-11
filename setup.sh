c1e53d27c052fa724b05a0d79177006fad61a73f
matijepekovic <122064226+matijepekovic@users.noreply.github.com>
2025-06-10 17:54:36 -0700

    Update setup script for Flutter 3.32

--- setup.sh
+++ setup.sh
@@ -4,10 +4,13 @@ set -euo pipefail
 apt-get update
 apt-get install -y curl git unzip xz-utils
 
-curl -o flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.19.0-stable.tar.xz
+curl -o flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.32.2-stable.tar.xz
 mkdir -p "$HOME"
 tar xf flutter.tar.xz -C "$HOME"
-export PATH="$PATH:$HOME/flutter/bin"
+export PATH="$HOME/flutter/bin:$PATH"
+
+# Allow flutter's internal git repos to be used by root
+git config --global --add safe.directory "$HOME/flutter"
 
 flutter doctor
 flutter pub get
